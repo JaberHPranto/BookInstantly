8aee19744a93a0be8c1b2b19dcab4b16eda41d43
JaberHPranto <jabearhossain@iut-dhaka.edu>
2022-05-17 21:50:04 +0600

    🥳 initial setup done

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongodb connected !!!");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  connect();
+  console.log(`Server started on port ${PORT} 😎`);
+});
